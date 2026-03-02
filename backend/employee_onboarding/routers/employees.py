c871edac04e3c61e11d8c427d65063f035fa8dc0
shahdhoss <s-shahd.fouda@zewailcity.edu.eg>
2026-03-02 23:28:08 +0200

    merging employee onboarding branch

--- backend/employee_onboarding/routers/employees.py
+++ backend/employee_onboarding/routers/employees.py
@@ -5,18 +5,26 @@ from database import db_dependency
 from sqlalchemy import select
 import uuid
 from ..service.employees import EmployeeService
+from ..service.employee_images import EmployeeImagesService
 from ..repository.employee import EmployeeRepository
 from ..exceptions import EmployeeNotFound
 from database import get_db
 from sqlalchemy.ext.asyncio import AsyncSession
-
+from ..utils.minio_storage_client import MinioStorageClient
 
 employee_router = APIRouter(prefix="/employee", tags=["employees"])
+
+def get_minio_client():
+    return MinioStorageClient()
+
+def get_employee_image_service(object_storage_client: MinioStorageClient = Depends(get_minio_client)):
+    return EmployeeImagesService(minio_client=object_storage_client)
+
 async def get_employee_repository(db: AsyncSession = Depends(get_db)):
     return EmployeeRepository(db=db)
 
-async def get_employee_service(repo: EmployeeRepository = Depends(get_employee_repository)):
-    return EmployeeService(repo)
+async def get_employee_service(repo: EmployeeRepository = Depends(get_employee_repository), object_storage:MinioStorageClient = Depends(get_minio_client), employee_image_service: EmployeeImagesService=Depends(get_employee_image_service)):
+    return EmployeeService(repository=repo, object_storage=object_storage, employee_image_service=employee_image_service)
 
 # reads all employees info
 @employee_router.get("", response_model=list[EmployeeResponse])
@@ -30,9 +38,12 @@ async def get_all_employees(service: EmployeeService = Depends(get_employee_serv
 #creates a new employee
 @employee_router.post("", response_model=EmployeeResponse , status_code=status.HTTP_201_CREATED) 
 async def create_employee(first_name: str = Form(...), last_name: str = Form(...), role: str = Form(...), employee_pictures: list[UploadFile] = File(...), service: EmployeeService = Depends(get_employee_service)):
-    employee = EmployeeCreate(first_name=first_name, last_name= last_name, role= role)
-    created_employee = await service.create_employee(employee=employee, employee_pictures=employee_pictures)
-    return created_employee
+    try: 
+        employee = EmployeeCreate(first_name=first_name, last_name= last_name, role= role)
+        created_employee = await service.create_employee(employee=employee, employee_pictures=employee_pictures)
+        return created_employee
+    except Exception as error:
+        raise error
 
 # gets an employee by id 
 @employee_router.get("/{id}", response_model=EmployeeResponse)
